f76bdb3cda625ace4872db54ed0c7d341e8870c5
frank <frank@franks-MacBook-Pro.local>
2020-08-16 16:13:57 +0530

    Core Burger Builder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4,
+        purchasable: false,
+        purchasing: false
+    }
+    render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+        return (
+            <Aux>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHander}>
+                    <OrderSummary
+                        price={this.state.totalPrice}
+                        ingredients={this.state.ingredients}
+                        purchaseCancelHander={this.purchaseCancelHander}
+                        purchaseContinueHander={this.purchaseContinueHander}
+                    />
+                </Modal>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHanlder}
+                    ingredientRemoved={this.removeIngredientHanlder}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler} />
+            </Aux>
+        );
+    }
+
+    purchaseCancelHander = () => {
+        this.setState({
+            purchasing: false
+        });
+    }
+
+    purchaseContinueHander = () => {
+        this.setState({
+            purchasing: false
+        });
+        console.log("Order Continued");
+    }
+
+    addIngredientHanlder = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredient = {
+            ...this.state.ingredients,
+        }
+        updatedIngredient[type] = updatedCount;
+
+        const oldPrice = this.state.totalPrice;
+        const priceAddition = INGREDIENT_PRICES[type];
+
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredient
+        });
+        this.updatePurchaseState(updatedIngredient);
+    }
+
+    removeIngredientHanlder = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredient = {
+            ...this.state.ingredients,
+        }
+        updatedIngredient[type] = updatedCount;
+
+        const oldPrice = this.state.totalPrice;
+        const priceAddition = INGREDIENT_PRICES[type];
+
+        const newPrice = oldPrice - priceAddition;
+
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredient
+        });
+        this.updatePurchaseState(updatedIngredient);
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({
+            purchasable: sum > 0
+        })
+    }
+
+    purchaseHandler = () => {
+        this.setState({
+            purchasing: true
+        })
+    }
+
+}
+
+export default BurgerBuilder;
